8b5dc24744c370a2d57fcc45aa4a043447481240
Ryan Miller <ryanmillergm@gmail.com>
2026-05-12 12:22:05 -0600

    Adds firebase auth to login and sign up pages

--- lib/signup_page.dart
+++ lib/signup_page.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_tutorial/login_page.dart';
 
@@ -21,6 +22,19 @@ class _SignUpPageState extends State<SignUpPage> {
     emailController.dispose();
     passwordController.dispose();
     super.dispose();
+  }
+
+  Future<void> createUserWithEmailAndPassword() async {
+    try {
+      final userCredential = 
+        await FirebaseAuth.instance.createUserWithEmailAndPassword(
+          email: emailController.text.trim(), 
+          password: passwordController.text.trim()
+      );
+      print(userCredential.user?.uid);
+    } on FirebaseAuthException catch (e) {
+      print(e.message);
+    }
   }
 
   @override
@@ -58,7 +72,9 @@ class _SignUpPageState extends State<SignUpPage> {
               ),
               const SizedBox(height: 20),
               ElevatedButton(
-                onPressed: () {},
+                onPressed: () async {
+                  await createUserWithEmailAndPassword();
+                },
                 child: const Text(
                   'SIGN UP',
                   style: TextStyle(
